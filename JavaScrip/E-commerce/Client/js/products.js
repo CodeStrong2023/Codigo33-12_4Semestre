66c8bb9bec34e658f91a8a03acc246b40ec3de39
Jessica Pagano <jeessieh@hotmail.com>
2024-08-26 09:21:01 -0300

    SEMANA 1
    clase1

---
+++ JavaScrip/E-commerce/Client/js/products.js
@@ -0,0 +1,31 @@
+const productos = [
+    {
+        id: 1,
+        productName: "BUZO",
+        price: 500,
+        quanty: 1,
+        img: "/Client/media/buzo.jpg",
+    },
+    {
+        id: 2,
+        productName: "CAMPERA",
+        price: 1200,
+        quanty: 1,
+        img: "/Client/media/campera.jpg",
+    },
+    {
+        id: 3,
+        productName: "ENTERITO",
+        price: 800,
+        quanty: 1,
+        img: "/Client/media/enterito.jpg",
+    },
+    {
+        id: 4,
+        productName: "VESTIDO",
+        price: 1000,
+        quanty: 1,
+        img: "/Client/media/vestido.jpg",
+    }
+    
+]
